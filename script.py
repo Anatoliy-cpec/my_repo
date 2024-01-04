198bcd3dfa56cd0e70d228c2d0b5bf4317163a27
Anisiya <139621912+a4168048@users.noreply.github.com>
2024-01-04 23:28:06 +0300

    Code review
    Co-authored-by: Ruslan Senatorov <ruslansenatorov@users.noreply.github.com>

--- script.py
+++ script.py
@@ -1,117 +1,115 @@
 from dataclasses import dataclass
 import random
+from typing import List, Any
+
 
 @dataclass
 class Point:
     x: int = 0
     y: int = 0
 
-def __eq__(self, other) -> bool:
-     return self.x == other.x and self.y == other.y
+
+def __eq__(self, other: Any) -> bool:
+    return self.x == other.x and self.y == other.y
+
 
 class MyException(Exception):
-    def __str__(self):
-        return f'(My exception class)'
+    def __str__(self) -> str:
+        return f"(My exception class)"
+
 
 class OutOfRange(MyException):
-    def __str__(self):
-        return f'(Out of range)'
-    
+    def __str__(self) -> str:
+        return f"(Out of range)"
+
+
 class OccupiedPoint(MyException):
-    def __str__(self):
-        return f'(Point is occupied)'
+    def __str__(self) -> str:
+        return f"(Point is occupied)"
 
 
 class Player:
-    def __init__(self, board) -> None:
-        self.ships = []
-        self.ship_points = []
-        self.ship_count = 7
-        self.board = board
-    def add_ship(self, point):
+    def __init__(self, board: "Board") -> None:
+        self.ships: List[Point] = []
+        self.ship_points: List[Point] = []
+        self.ship_count: int = 7
+        self.board: "Board" = board
+
+    def add_ship(self, point: Point) -> None:
         self.ships.append(point)
-    def add_points(self):
+
+    def add_points(self) -> None:
         pass
-    def get_points(self):
+
+    def get_points(self) -> None:
         pass
-    
+
 
 class AI(Player):
-    def shoot():
+    def shoot(self) -> None:
         pass
 
 
 class User(Player):
-    def shoot():
+    def shoot(self) -> None:
         pass
 
+
 class Ship:
-    def __init__(self, size, align, hp) -> None:
-        self.__size = size
-        self.__align = align
-        self.__hp = hp
+    def __init__(self, size: int, align: str, hp: int) -> None:
+        self.__size: int = size
+        self.__align: str = align
+        self.__hp: int = hp
 
 
 class Board:
-    def __init__(self, size = 6) -> None:
-        self.__size = size
-        self.__field = [['o' for i in range(self.__size)] for j in range(self.__size)]
+    def __init__(self, size: int = 6) -> None:
+        self.__size: int = size
+        self.__field: List[List[str]] = [
+            ["o" for i in range(self.__size)] for j in range(self.__size)
+        ]
 
-    def create_ship(self):
-        _i = True
+    def create_ship(self) -> None:
+        _i: bool = True
         while _i:
             try:
-                # comment: 
+                ...
             except Exception as e:
-                raise
+                raise Exception(e)
             else:
-                # comment: 
+                ...
             finally:
-                # comment: 
+                ...
             # end try
 
-    def create_ships(self, player):
-        for i in player.ship_count:
-            player.add_ship(self.create_ship(self))
+    def create_ships(self, player: Player) -> None:
+        for i in range(player.ship_count):
+            player.add_ship(self.create_ship())
 
-
-    def add_ships_to_board(self):
+    def add_ships_to_board(self) -> None:
         pass
-        
-                   
-
-
-
-        
-
-
-
-
-
 
     @property
-    def drow_board(self):
+    def drow_board(self) -> None:
         print()
-        _str = '__|'
+        _str: str = "__|"
         for i in range(self.__size):
-            _str += f' {i+1} |'
+            _str += f" {i+1} |"
         print(_str)
-        _str = '  |'
+        _str = "  |"
         for i in range(self.__size):
-            _str = f'{i+1} |'
+            _str = f"{i+1} |"
             for j in range(self.__size):
-                _str += f' {self.__field[i][j]} |'
+                _str += f" {self.__field[i][j]} |"
             print(_str)
         print()
 
-b1 = Board(6)
-p1 = Player(b1)
+
+b1: Board = Board(6)
+p1: Player = Player(b1)
 
 p1.board.drow_board
 p1.board.add_ships(p1)
-
-
-
 
 
 class Game:
